0b1b45dbca2c84d29484ab1da44806a3211d121e
Adrian Low <adrianlow1998@gmail.com>
2025-04-02 18:47:48 +0800

    updated test to test expected output

--- tests/test_event.py
+++ tests/test_event.py
@@ -3,39 +3,72 @@ from json import loads
 from models import Event
 
 class TestEventModel(unittest.TestCase):
-    def setUp(self):
-        self.phx_reply_ok = """{"ref":"1","payload":{"status":"ok","response":{"service":{"id":1,"name":"Service 1"}}},"topic":"backend/service/1","event":"phx_reply"}"""
-        self.phx_reply_error = """{"ref":"1","payload":{"status":"error","response":{"reason":"Service not found"}},"topic":"backend/service/1","event":"phx_reply"}"""
-        self.service_updated = """{"ref":null,"payload":{"service":{"id":1,"name":"Service 1"}},"topic":"backend/service/1","event":"service_updated"}"""
-        self.service_deleted = """{"ref":null,"payload":{"service":{"id":1,"name":"Service 1"}},"topic":"backend/service/1","event":"service_deleted"}"""
-
     def test_phx_reply_ok(self):
-        try:
-            Event(**loads(self.phx_reply_ok))
-            self.assertTrue(True)
-        except Exception as e:
-            self.assertTrue(False)
+        input = """{"ref":"1","payload":{"status":"ok","response":{"service":{"id":1,"name":"Service 1"}}},"topic":"backend/service/1","event":"phx_reply"}"""
+        expected = {
+            "ref": "1",
+            "topic": "backend/service/1",
+            "event": "phx_reply",
+            "payload": {
+                "status": "ok",
+                "response": {
+                    "service": {
+                        "id": 1,
+                        "name": "Service 1"
+                    }
+                }
+            }
+        }
+        actual = Event(**loads(input)).model_dump()
+        self.assertEqual(actual, expected)
 
     def test_phx_reply_error(self):
-        try:
-            Event(**loads(self.phx_reply_error))
-            self.assertTrue(True)
-        except Exception as e:
-            self.assertTrue(False)
+        input = """{"ref":"1","payload":{"status":"error","response":{"reason":"Service not found"}},"topic":"backend/service/1","event":"phx_reply"}"""
+        expected = {
+            "ref": "1",
+            "topic": "backend/service/1",
+            "event": "phx_reply",
+            "payload": {
+                "status": "error",
+                "response": {
+                    "reason": "Service not found"
+                }
+            }
+        }
+        actual = Event(**loads(input)).model_dump()
+        self.assertEqual(actual, expected)
 
     def test_service_updated(self):
-        try:
-            Event(**loads(self.service_updated))
-            self.assertTrue(True)
-        except Exception as e:
-            raise Exception(e)
+        input = """{"ref":null,"payload":{"service":{"id":1,"name":"Service 1"}},"topic":"backend/service/1","event":"service_updated"}"""
+        expected = {
+            "ref": None,
+            "topic": "backend/service/1",
+            "event": "service_updated",
+            "payload": {
+                "service": {
+                    "id": 1,
+                    "name": "Service 1"
+                }
+            }
+        }
+        actual = Event(**loads(input)).model_dump()
+        self.assertEqual(actual, expected)
 
     def test_service_deleted(self):
-        try:
-            Event(**loads(self.service_deleted))
-            self.assertTrue(True)
-        except Exception as e:
-            raise Exception(e)
+        input = """{"ref":null,"payload":{"service":{"id":1,"name":"Service 1"}},"topic":"backend/service/1","event":"service_deleted"}"""
+        expected = {
+            "ref": None,
+            "topic": "backend/service/1",
+            "event": "service_deleted",
+            "payload": {
+                "service": {
+                    "id": 1,
+                    "name": "Service 1"
+                }
+            }
+        }
+        actual = Event(**loads(input)).model_dump()
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
