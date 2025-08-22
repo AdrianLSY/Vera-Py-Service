4005dcaeb579733b4651b9febefc9a805411fc9a
Adrian Low <adrianlow1998@gmail.com>
2025-08-22 17:57:23 +0800

    Update action_response.py

--- core/action_response.py
+++ core/action_response.py
@@ -1,7 +1,12 @@
 from typing import Union
-from pydantic import BaseModel
+from pydantic import Field
+from core.action_model import ActionModel
 
-class ActionResponse(BaseModel):
-    status_code: int
-    message: Union[str, None] = None
-    fields: Union[str, int, float, bool, dict, None] = None
+class ActionResponse(ActionModel):
+    status_code: int = Field(description = "The HTTP status code of the response")
+    message: Union[str, None] = Field(description = "The message of the response", default = None)
+    fields: Union[str, int, float, bool, dict, None] = Field(description = "The fields of the response", default = None)
+
+    @classmethod
+    def description(cls) -> str:
+        return "The model used as a standard response returned by ActionRunner.run()"
